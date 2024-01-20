c11d8c14ab89581f50e80a87f761e1f334ef67c5
YMStudio-youssef <133716171+YMStudio-youssef@users.noreply.github.com>
2024-01-20 15:07:35 +0300

    commit 01

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // Function to toggle responsive menu
+  function toggleMenu() {
+    $(".responsive-menu").slideToggle();
+  }
+
+  // Event listener for menu icon click
+  $(".menu-bar").click(function() {
+    toggleMenu();
+  });
+});
